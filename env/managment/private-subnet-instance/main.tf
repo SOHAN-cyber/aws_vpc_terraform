84341a53f7c48d320f8d659971ea11e80d982b93
SOHAN-cyber <sohandogra703@gmail.com>
2022-11-05 18:58:02 +0530

    private subnet instance

--- env/managment/private-subnet-instance/main.tf
+++ env/managment/private-subnet-instance/main.tf
@@ -20,18 +20,6 @@ resource "aws_instance" "private-server" {
   tags = {
     "Name" = "jump-server"
   }
-provisioner "file" {
-source = "./script.sh"   #this is the source for copying the file 
-destination = "/home/ubuntu/script.sh"   #this is for destion of file to be copied
-}
-
-connection {
-type = "ssh"
-user = "ubuntu"
-host = aws_instance.private-server[count.index].public_ip  #this is for login into the instance app-server
-private_key = file("./private-subnet")   #this is the location for the key of the instance
-timeout = "4m"  #after 4 minute if instance is not able to lofin then it will show timeout error
-}
 }
 
 resource "aws_security_group" "private-server" {
