5769f95c412277439a93fcfc66a1dad3f0843fc6
SOHAN-cyber <sohandogra703@gmail.com>
2022-11-05 18:32:40 +0530

    added publ

--- env/managment/private-subnet-instance/main.tf
+++ env/managment/private-subnet-instance/main.tf
@@ -28,7 +28,7 @@ destination = "/home/ubuntu/script.sh"   #this is for destion of file to be copi
 connection {
 type = "ssh"
 user = "ec2-user"
-host = aws_instance.private-server.public_ip  #this is for login into the instance app-server
+host = aws_instance.private-server[count.index].public_ip  #this is for login into the instance app-server
 private_key = file("../private-subnet")   #this is the location for the key of the instance
 timeout = "4m"  #after 4 minute if instance is not able to lofin then it will show timeout error
 }
