5cfc64c31b14a252285aa8f355809b74639d4a5e
SOHAN-cyber <sohandogra703@gmail.com>
2022-11-05 20:03:20 +0530

    add

--- env/managment/alb/main.tf
+++ env/managment/alb/main.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "my-lb" {
   internal = false 
   load_balancer_type = "application"
   security_group = [aws_security_group.alb.id]
-  subnets = [data.terraform_remote_state.network.outputs.public_subnet[0][0], data.terraform_remote_state.network.outputs.public_subnet[0][1]]
+  subnets = [data.terraform_remote_state.vpc.outputs.public_subnet[0][0], data.terraform_remote_state.vpc.outputs.public_subnet[0][1]]
   enable_deletion_protection = false
   enable_cross_zone_load_balancing =  true
   ip_address_type = "dualstack"
@@ -34,12 +34,32 @@ resource "aws_security_group" "alb" {
 }
 
 
-# resource "aws_lb_listener" "front_end_80" {
-#   load_balancer_arn = aws_lb.my-lb.arn
-#   port              = "80"
-#   protocol          = "HTTP"
+resource "aws_lb_listener" "front_end_80" {
+  load_balancer_arn = aws_lb.my-lb.arn
+  port              = "80"
+  protocol          = "HTTP"
 
-#   default_action {
-#     type = "redirect"
+  default_action {
+    type = "fixed-response"
 
-# }
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "HEALTHY"
+      status_code  = "200"
+    }
+
+}
+
+resource "aws_lb_listener_rule" "my-domain" {
+  listener_arn = aws_lb_listener.front_end_80.arn
+  action {
+    type = "forward"
+    target_group_arn = data.terraform_remote_state.private_ec2_instance.outputs.aws_lb_target_group.arn
+  }
+
+  condition {
+    host_header {
+      values = ["sohan.com"]
+    }
+  }
+}
--- env/managment/private-subnet-instance/output.tf
+++ env/managment/private-subnet-instance/output.tf
@@ -1,3 +1,11 @@
 output "private_ec2_instance" {
   value = aws_instance.private-server.*.id
 }
+
+output "target_group_arn" {
+  value = aws_lb_target_group.private-subnet-instance.arn
+}
+
+output "target_group_id" {
+  value = aws_lb_target_group.private-subnet-instance.id
+}
